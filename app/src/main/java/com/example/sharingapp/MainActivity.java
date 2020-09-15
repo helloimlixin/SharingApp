ecd37bad215fb7019f5bbb1cfdfacb4e494eb734
helloimlixin <helloimlixin@gmail.com>
2020-09-15 09:29:28 +0800

    1. Cleared the App memory from previous operations. 2. Added a menu folder and the corresponding xml file. 3. Added overriding methods `onCreateOptionsMenu()` and `onOptionsItemSelected()`. 3. Initialized the Android activity `ContactsActivity`.

--- app/src/main/java/com/example/sharingapp/MainActivity.java
+++ app/src/main/java/com/example/sharingapp/MainActivity.java
@@ -40,4 +40,35 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, AddItemActivity.class);
         startActivity(intent);
     }
+
+    /**
+     * This method is called when <code>MainActivity</code> is started. This method links the menu
+     * resource file {@link /src/main/res/menu/menu_main.xml} to {@link MainActivity} and "inflates" the
+     * menu.
+     * @param menu menu item to load
+     * @return boolean value representing load status
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu); // menu resource, menu
+        return true;
+    }
+
+    /**
+     * This method is called when the user selects an option from the menu. In this application, this
+     * code handles what happens when the user selects the "Contacts" option from the menu --
+     * {@link ContactsActivity} is started.
+     * @param item menu item selected
+     * @return boolean value indicating the status of the app handling the item selection.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.contacts:
+                Intent intent = new Intent(this, ContactsActivity.class);
+
+        }
+        return true;
+    }
 }
